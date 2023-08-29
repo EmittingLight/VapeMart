34d9a7de81b4c885679e53c14ec487d757ed56e4
SergeiPetrov1974 <58001369+SergeiPetrov1974@users.noreply.github.com>
2023-08-29 16:48:31 +0300

    Создал класс CustomerController, CustomerRepository, CustomerService, добавил в properties # Server port configuration  server.port=8081. Запускал приложение, вроде не падает.

---
+++ src/main/java/com/yaga/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.yaga.repository;
+
+import com.yaga.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
